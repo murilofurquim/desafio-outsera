1fc6ad9e8908fa8d986ff288972de39e6a7bcde5
Murilo <murilofurquim@gmail.com>
2025-04-05 19:50:17 -0300

    busca dos dados min e max

--- src/database/database.py
+++ src/database/database.py
@@ -2,7 +2,7 @@ import csv
 
 import jaydebeapi
 
-def init(movies_csv: str):
+def get_conn():
     h2_jar = "resources/h2-2.3.232.jar"
     url = "jdbc:h2:~/test"
 
@@ -12,9 +12,13 @@ def init(movies_csv: str):
         ["", ""],
         h2_jar,
     )
+    return conn
 
+def init(movies_csv: str):
+    conn = get_conn()
     cursor = conn.cursor()
 
+    cursor.execute("DROP TABLE IF EXISTS awards")
     cursor.execute("""CREATE TABLE IF NOT EXISTS awards (
     award_year INT,
     title VARCHAR(255),
@@ -24,14 +28,48 @@ def init(movies_csv: str):
 );""")
 
     with open(movies_csv, newline='', encoding='utf-8') as csvfile:
+        total = 0
         reader = csv.reader(csvfile, delimiter=';')
         next(reader)
         for row in reader:
-            cursor.execute("INSERT INTO movies (award_year, title, studios, producers, winner) VALUES (?, ?, ?, ?, ?)", row)
-
-    cursor.execute("SELECT * FROM movies")
-    result = cursor.fetchall()
-    print(result)
+            total = total + 1
+            cursor.execute("INSERT INTO awards (award_year, title, studios, producers, winner) VALUES (?, ?, ?, ?, ?)",
+                           row)
 
     cursor.close()
+    conn.commit()
     conn.close()
+
+def get_dados():
+    conn = get_conn()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("""
+       with intervalos as (
+    select producers, AWARD_YEAR, LAG(AWARD_YEAR, 1, null) over (partition by PRODUCERS order by AWARD_YEAR) as anterior,
+            AWARD_YEAR - LAG(AWARD_YEAR, 1, null) over (partition by PRODUCERS order by AWARD_YEAR) as diferenca
+           from awards
+           where winner = 'yes'
+)
+select 'min' as descricao, i.PRODUCERS, i.AWARD_YEAR, i.anterior, i.diferenca
+    from intervalos i
+where i.diferenca = (select min(diferenca) from intervalos)
+union all
+select 'max' as descricao, i.PRODUCERS, i.AWARD_YEAR, i.anterior, i.diferenca
+    from intervalos i
+where i.diferenca = (select max(diferenca) from intervalos)
+        """)
+        result = cursor.fetchall()
+
+        if len(result) == 0:
+            return [],[]
+
+        min = [x[1:] for x in filter(lambda x: x[0] == 'min', result)]
+        max = [x[1:] for x in filter(lambda x: x[0] == 'max', result)]
+
+        return min, max
+    finally:
+        cursor.close()
+        conn.close()
+
--- src/main.py
+++ src/main.py
@@ -3,11 +3,11 @@ from routes import raspberry_awards
 import database.database
 import uvicorn
 
-
-database.database.init("resources/movielist.csv")
 app = FastAPI()
 app.include_router(raspberry_awards.router)
 
 
 if __name__ == "__main__":
+    database.database.init("resources/movielist.csv")
+    print(database.database.get_dados())
     uvicorn.run(app, host="0.0.0.0", port=8000)
